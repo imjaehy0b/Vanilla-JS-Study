2e25d27e838d81ee042abee500c7b653a9a272aa
imjaehyub <imjae950@gmail.com>
2024-08-08 09:08:07 +0900

    add count js,html

---
+++ 02-counter/app.js
@@ -0,0 +1,60 @@
+const number = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+let cnt = 0;
+
+
+
+btns.forEach(function (btn){
+  btn.addEventListener('click', function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("dec")) {
+      cnt--;
+    } else if (styles.contains("inc")) {
+      cnt++;
+    } else {
+      cnt = 0;
+    }
+
+    if (cnt > 0) {
+      number.style.color = "green";
+    } else if (cnt < 0) {
+      number.style.color = "red";
+    } else {
+      number.style.color = "black";
+    }
+
+    number.textContent = cnt;
+  })
+});
+
+
+// my code
+// function decrementNumber() {
+//   --cnt;
+//   number.textContent = cnt;
+// }
+// function incrementNumber() {
+//   ++cnt;
+//   number.textContent = cnt;
+// }
+// function resetNumber() {
+//   cnt = 0;
+//   number.textContent = cnt;
+// }
+
+// for (const button of btns) {
+//   switch (button.classList[1]) {
+//     case "dec":
+//       button.addEventListener("click", decrementNumber);
+//       break;
+//     case "reset":
+//       button.addEventListener("click", resetNumber);
+//       break;
+//       case "inc":
+//       button.addEventListener("click", incrementNumber);
+//       break;
+//     default:
+//       break;
+//   }
+// }
